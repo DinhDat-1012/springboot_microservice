2b2fd793f24d789d67a6ff3fddb6434bd8758e0a
Đinh Đức Đạt <91339985+DinhDat-1012@users.noreply.github.com>
2024-10-15 01:53:27 +0700

    user data base created

---
+++ user_service/user_service/src/main/java/com/microservice_prj/user_service/Model/User.java
@@ -0,0 +1,26 @@
+package com.microservice_prj.user_service.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity()
+@Table(name = "user_db")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+}
